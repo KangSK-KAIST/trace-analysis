a4a37df004410fc762830730479cdffa8b817be4
KangSK-KAIST <steelyk7@gmail.com>
2021-04-03 21:58:27 +0900

    Added license

--- src/reader/raw_file_reader.cc
+++ src/reader/raw_file_reader.cc
@@ -1,3 +1,25 @@
+// MIT License
+
+// Copyright (c) 2021 KangSK-KAIST
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include "raw_file_reader.hh"
 
 int read_trace(std::string fileName, std::vector<TraceData> *vTraceData) {
@@ -28,7 +50,7 @@ int read_trace(std::string fileName, std::vector<TraceData> *vTraceData) {
   // Sort the array, just in case
   std::sort(vTraceData->begin(), vTraceData->end());
 
-  // Assing an unique id
+  // Assign an unique id
   uint32_t count = 0;
   for (auto iter = vTraceData->begin(); iter < vTraceData->end(); iter++) {
     iter->id = count++;
