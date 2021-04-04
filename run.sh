a8912f6235852f4df20adbd119106187045e5145
KangSK-KAIST <steelyk7@gmail.com>
2021-04-04 23:37:12 +0900

    Modified run script and corresponding gitignore

--- run.sh
+++ run.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
-rm -r results
+# build/main_snia /mnt/disk0/snia_refined/MSProduction/24Hour_RADIUS.total.csv.orig 8192 > results-8G/24Hour_RADIUS &
+# build/main_snia /mnt/disk0/snia_refined/MSProduction/MSNFS.total.csv.orig 8192 > results-8G/MSNFS &
+# build/main_snia /mnt/disk0/snia_refined/MSProduction/24Hour_RADIUS_SQL.total.csv.orig 8192 > results-8G/24Hour_RADIUS_SQL &
+# build/main_snia /mnt/disk0/snia_refined/MSProduction/BuildServer.total.csv.orig 8192 > results-8G/BuildServer &
+# build/main_snia /mnt/disk0/snia_refined/MSProduction/CFS.total.csv.orig 8192 > results-8G/CFS &
+# build/main_snia /mnt/disk0/snia_refined/MSProduction/DisplayAdsPayload.total.csv.orig 8192 > results-8G/DisplayAdsPayload &
+# build/main_snia /mnt/disk0/snia_refined/MSProduction/DevDivRelease.total.csv.orig 8192 > results-8G/DevDivRelease &
+# build/main_snia /mnt/disk0/snia_refined/MSEnterprise/Exchange.total.csv.orig 8192 > results-8G/Exchange &
+# build/main_snia /mnt/disk0/snia_refined/MSRCambridge/prn.total.csv.orig 8192 > results-8G/prn &
+# build/main_snia /mnt/disk0/snia_refined/MSRCambridge/src1.total.csv.orig 8192 > results-8G/src1 &
+# build/main_snia /mnt/disk0/snia_refined/MSRCambridge/src2.total.csv.orig 8192 > results-8G/src2 &
+# build/main_snia /mnt/disk0/snia_refined/MSRCambridge/ts.total.csv.orig 8192 > results-8G/ts &
+# build/main_snia /mnt/disk0/snia_refined/MSRCambridge/wdev.total.csv.orig 8192 > results-8G/wdev &
+# build/main_snia /mnt/disk0/snia_refined/FIU-srcmap/ikki.total.csv.orig 8192 > results-8G/ikki &
+# build/main_snia /mnt/disk0/snia_refined/FIU-srcmap/webmail+online.total.csv.orig 8192 > results-8G/ikki &
 
-build/main_snia /mnt/disk0/snia_refined/MSProduction/24Hour_RADIUS_SQL.total.csv.orig 4096 > results/24Hour_RADIUS_SQL &
-build/main_snia /mnt/disk0/snia_refined/MSProduction/BuildServer.total.csv.orig 4096 > results/BuildServer &
-build/main_snia /mnt/disk0/snia_refined/MSProduction/CFS.total.csv.orig 4096 > results/CFS &
-build/main_snia /mnt/disk0/snia_refined/MSProduction/DisplayAdsPayload.total.csv.orig 4096 > results/DisplayAdsPayload &
-build/main_snia /mnt/disk0/snia_refined/MSProduction/DevDivRelease.total.csv.orig 4096 > results/DevDivRelease &
-build/main_snia /mnt/disk0/snia_refined/MSEnterprise/Exchange.total.csv.orig 4096 > results/Exchange &
-build/main_snia /mnt/disk0/snia_refined/MSProduction/MSNFS.total.csv.orig 4096 > results/MSNFS &
-build/main_snia /mnt/disk0/snia_refined/MSRCambridge/prn.total.csv.orig 4096 > results/prn &
-build/main_snia /mnt/disk0/snia_refined/MSRCambridge/src1.total.csv.orig 4096 > results/src1 &
-build/main_snia /mnt/disk0/snia_refined/MSRCambridge/src2.total.csv.orig 4096 > results/src2 &
-build/main_snia /mnt/disk0/snia_refined/MSRCambridge/ts.total.csv.orig 4096 > results/ts &
-build/main_snia /mnt/disk0/snia_refined/MSRCambridge/wdev.total.csv.orig 4096 > results/wdev &
-
-build/main_ali /mnt/disk0/alibaba_block_traces_2020/dev3/trace.csv 4096 > results/dev3 &
-build/main_ali /mnt/disk0/alibaba_block_traces_2020/dev64/trace.csv 4096 > results/dev64 &
-build/main_ali /mnt/disk0/alibaba_block_traces_2020/dev125/trace.csv 4096 > results/dev125 &
-build/main_ali /mnt/disk0/alibaba_block_traces_2020/dev550/trace.csv 4096 > results/dev550 &
-build/main_ali /mnt/disk0/alibaba_block_traces_2020/dev667/trace.csv 4096 > results/dev667 &
-build/main_ali /mnt/disk0/alibaba_block_traces_2020/dev669/trace.csv 4096 > results/dev669 &
-build/main_ali /mnt/disk0/alibaba_block_traces_2020/dev726/trace.csv 4096 > results/dev726 &
-build/main_ali /mnt/disk0/alibaba_block_traces_2020/dev743/trace.csv 4096 > results/dev743 &
-build/main_ali /mnt/disk0/alibaba_block_traces_2020/dev767/trace.csv 4096 > results/dev767 &
-build/main_ali /mnt/disk0/alibaba_block_traces_2020/dev852/trace.csv 4096 > results/dev852 &
-build/main_ali /mnt/disk0/alibaba_block_traces_2020/dev907/trace.csv 4096 > results/dev907 &
-build/main_ali /mnt/disk0/alibaba_block_traces_2020/dev972/trace.csv 4096 > results/dev972
+build/main_ali /mnt/disk0/alibaba_block_traces_2020/dev49/trace.csv 8192 > results-8G/dev49 &
+build/main_ali /mnt/disk0/alibaba_block_traces_2020/dev95/trace.csv 8192 > results-8G/dev95 &
+build/main_ali /mnt/disk0/alibaba_block_traces_2020/dev125/trace.csv 8192 > results-8G/dev125 &
+build/main_ali /mnt/disk0/alibaba_block_traces_2020/dev308/trace.csv 8192 > results-8G/dev308 &
+build/main_ali /mnt/disk0/alibaba_block_traces_2020/dev355/trace.csv 8192 > results-8G/dev355 &
+build/main_ali /mnt/disk0/alibaba_block_traces_2020/dev373/trace.csv 8192 > results-8G/dev373 &
+build/main_ali /mnt/disk0/alibaba_block_traces_2020/dev454/trace.csv 8192 > results-8G/dev454 &
+build/main_ali /mnt/disk0/alibaba_block_traces_2020/dev457/trace.csv 8192 > results-8G/dev457 &
+build/main_ali /mnt/disk0/alibaba_block_traces_2020/dev467/trace.csv 8192 > results-8G/dev467 &
+build/main_ali /mnt/disk0/alibaba_block_traces_2020/dev477/trace.csv 8192 > results-8G/dev477 &
+build/main_ali /mnt/disk0/alibaba_block_traces_2020/dev550/trace.csv 8192 > results-8G/dev550 &
+build/main_ali /mnt/disk0/alibaba_block_traces_2020/dev554/trace.csv 8192 > results-8G/dev554 &
+build/main_ali /mnt/disk0/alibaba_block_traces_2020/dev618/trace.csv 8192 > results-8G/dev618 &
+build/main_ali /mnt/disk0/alibaba_block_traces_2020/dev667/trace.csv 8192 > results-8G/dev667 &
+build/main_ali /mnt/disk0/alibaba_block_traces_2020/dev767/trace.csv 8192 > results-8G/dev767 &
+build/main_ali /mnt/disk0/alibaba_block_traces_2020/dev770/trace.csv 8192 > results-8G/dev770 &
