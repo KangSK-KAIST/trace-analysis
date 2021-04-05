b6d07e205de0509713146b2a866b4bea1281c94a
KangSK-KAIST <steelyk7@gmail.com>
2021-04-05 18:11:49 +0900

    Modified analyzer to analyze both reads and writes

--- src/analyzer/general_analyzer.cc
+++ src/analyzer/general_analyzer.cc
@@ -1,35 +1,43 @@
 #include "general_analyzer.hh"
 
-static void analyzeReadTypes(std::vector<TraceData>* vTraceData,
-                             std::map<id_t, std::set<id_t>>* mReadCentric,
-                             std::map<id_t, std::set<id_t>>* mWriteCentric,
-                             int32_t* indepReads, int32_t* depShortReads,
-                             int32_t* depLongReads, int32_t* writes) {
+static void analyzeDependTypes(std::vector<TraceData>* vTraceData,
+                               std::map<id_t, std::set<id_t>>* mCentric,
+                               bool isRead, int32_t* indep, int32_t* depShort,
+                               int32_t* depLong) {
   for (auto trace : (*vTraceData)) {
-    if (trace.isRead) {
-      if (mReadCentric->count(trace.id)) {
+    if (trace.isRead == isRead) {
+      if (mCentric->count(trace.id)) {
         // Exists a corresponding write
-        if ((*mReadCentric)[trace.id].size() > 1)
-          (*depLongReads)++;
+        if ((*mCentric)[trace.id].size() > 1)
+          (*depLong)++;
         else
-          (*depShortReads)++;
+          (*depShort)++;
       } else
-        (*indepReads)++;
-    } else
-      (*writes)++;
+        (*indep)++;
+    }
   }
 }
 
 void analyze(std::vector<TraceData>* vTraceData,
              std::map<id_t, std::set<id_t>>* mReadCentric,
              std::map<id_t, std::set<id_t>>* mWriteCentric) {
+  // Read breakdown
   int32_t indepReads = 0;     // Reads without correspoding writes
   int32_t depShortReads = 0;  // Reads connected to one write
   int32_t depLongReads = 0;   // Reads with multiple corresponding writes
-  int32_t writes = 0;         // Writes
-  analyzeReadTypes(vTraceData, mReadCentric, mWriteCentric, &indepReads,
-                   &depShortReads, &depLongReads, &writes);
-  std::cout << "Independent\tDep_Short\tDep_Long\tWrites" << std::endl;
-  std::cout << indepReads << "\t" << depShortReads << "\t" << depLongReads
-            << "\t" << writes << std::endl;
+  analyzeDependTypes(vTraceData, mReadCentric, true, &indepReads,
+                     &depShortReads, &depLongReads);
+  std::cout << "[Read BD]\tIndependent\tDep_Short\tDep_Long" << std::endl;
+  std::cout << "\t" << indepReads << "\t" << depShortReads << "\t"
+            << depLongReads << std::endl;
+
+  // Write breakdown
+  int32_t indepWrites = 0;     // Writes without correspoding writes
+  int32_t depShortWrites = 0;  // Writes connected to one write
+  int32_t depLongWrites = 0;   // Writes with multiple corresponding writes
+  analyzeDependTypes(vTraceData, mWriteCentric, false, &indepWrites,
+                     &depShortWrites, &depLongWrites);
+  std::cout << "[Write BD]\tIndependent\tDep_Short\tDep_Long" << std::endl;
+  std::cout << "\t" << indepReads << "\t" << depShortReads << "\t"
+            << depLongReads << std::endl;
 }
