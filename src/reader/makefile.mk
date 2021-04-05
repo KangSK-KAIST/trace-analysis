bab9ff9db9ffa4801aa4d2c2637f6a47c9c21255
KangSK-KAIST <steelyk7@gmail.com>
2021-04-05 15:42:51 +0900

    Removed full_read files - Instead, pass a zero size parameter

--- src/reader/makefile.mk
+++ src/reader/makefile.mk
@@ -1,11 +1,3 @@
-build/snia_reader.o: src/reader/snia_reader.cc
-	mkdir -p build
-	$(CC) $(CCFLAGS) -c -o build/snia_reader.o src/reader/snia_reader.cc
-
-build/snia_full_reader.o: src/reader/snia_full_reader.cc
-	mkdir -p build
-	$(CC) $(CCFLAGS) -c -o build/snia_full_reader.o src/reader/snia_full_reader.cc
-
 build/alibaba_reader.o: src/reader/alibaba_reader.cc
 	mkdir -p build
 	$(CC) $(CCFLAGS) -c -o build/alibaba_reader.o src/reader/alibaba_reader.cc
